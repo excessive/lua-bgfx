04945632b76a0264b84c8a46cd55825f135dcd64
Colby Klein <shakesoda@gmail.com>
2016-04-29 16:09:56 -0700

    Add test script.

---
+++ test.lua
@@ -0,0 +1,40 @@
+#!/usr/bin/env luajit
+package.cpath = package.cpath .. ";./bin/?.so"
+
+local bgfx = require "bgfx"
+
+bgfx.init(true)
+bgfx.reset(1280, 720, { "vsync" })
+bgfx.set_debug { "text" }
+
+-- broken
+-- local hmd = bgfx.get_hmd()
+-- for k, v in pairs(hmd) do
+-- 	print(k, v)
+-- end
+
+bgfx.set_view_clear(0, { "color", "depth" }, 0x303030ff, 1.0, 0)
+bgfx.set_view_name(0, "igor");
+
+-- local vb = bgfx.new_vertex_buffer()
+-- local ib = bgfx.new_index_buffer()
+-- local vsh = bgfx.new_shader()
+-- local fsh = bgfx.new_shader()
+-- local csh = bgfx.new_shader()
+-- local p = bgfx.new_program(vsh, fsh)
+
+for i=1,120 do
+	bgfx.debug_text_clear()
+	bgfx.debug_text_print(0, 0, 0x6f, "ayy lmao")
+	bgfx.set_view_rect(0, 0, 0, 1280, 720)
+	bgfx.touch(0)
+
+	-- bgfx.set_vertex_buffer(vb, 0, 30)
+	-- bgfx.set_index_buffer(ib, 0, 30)
+	-- bgfx.set_state { "pt_points" }
+	-- bgfx.submit(0, p)
+
+	bgfx.frame()
+end
+
+bgfx.shutdown()
