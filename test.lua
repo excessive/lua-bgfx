c6507a21ebe5ff53064f2f9fad882bf1c637ee1f
Colby Klein <shakesoda@gmail.com>
2016-04-30 14:00:40 -0700

    Update test

--- test.lua
+++ test.lua
@@ -18,39 +18,41 @@ end
 love.filesystem.init(get_low(arg))
 love.filesystem.setSource(love.filesystem.getWorkingDirectory())
 
-bgfx.init(true)
-bgfx.reset(1280, 720, { "vsync" })
-bgfx.set_debug { "text" }
+local function load()
+	bgfx.init(true)
+	bgfx.reset(1280, 720, { "vsync" })
+	bgfx.set_debug { "text" }
 
-local info = bgfx.get_renderer_info()
-print(info.name, info.type)
+	local info = bgfx.get_renderer_info()
+	print(info.name, info.type)
 
-local hmd = bgfx.get_hmd() or {}
-for k, v in pairs(hmd) do
-	print(k, v)
+	local hmd = bgfx.get_hmd() or {}
+	for k, v in pairs(hmd) do
+		print(k, v)
+	end
+
+	local fmt = bgfx.new_vertex_format {
+		{ attrib = "position", type = "float", num = 3 }
+	}
+	print(fmt)
+
+	local ib = bgfx.new_index_buffer({1, 2, 3, 4, 5, 6})
+	print(ib)
+
+	local data, err = love.filesystem.newFileData("bin/bgfx.so")
+	local vb = bgfx.new_vertex_buffer(data:getPointer(), data:getSize(), fmt)
+	print(vb)
+
+	bgfx.set_view_clear(0, { "color", "depth" }, 0x303030ff, 1.0, 0)
+	bgfx.set_view_name(0, "igor");
 end
-
-local fmt = bgfx.new_vertex_format {
-	{ attrib = "position", type = "float", num = 3 }
-}
-print(fmt)
-
-local ib = bgfx.new_index_buffer({1, 2, 3, 4, 5, 6})
-print(ib)
-
-local data, err = love.filesystem.newFileData("bin/bgfx.so")
-local vb = bgfx.new_vertex_buffer(data:getPointer(), data:getSize(), fmt)
-print(vb)
-
-bgfx.set_view_clear(0, { "color", "depth" }, 0x303030ff, 1.0, 0)
-bgfx.set_view_name(0, "igor");
 
 -- local vsh = bgfx.new_shader()
 -- local fsh = bgfx.new_shader()
 -- local csh = bgfx.new_shader()
 -- local p = bgfx.new_program(vsh, fsh)
 
-for i=1,120 do
+local function draw()
 	bgfx.debug_text_clear()
 	bgfx.debug_text_print(0, 0, 0x6f, "ayy lmao")
 	bgfx.set_view_rect(0, 0, 0, 1280, 720)
@@ -64,5 +66,49 @@ for i=1,120 do
 	bgfx.frame()
 end
 
+local function quit()
+	bgfx.shutdown()
+end
 
-bgfx.shutdown()
+require "love.math"
+require "love.event"
+require "love.timer"
+require "love.keyboard"
+
+love.math.setRandomSeed(os.time())
+love.event.pump()
+if load then load() end
+love.timer.step()
+local dt = 0
+while true do
+	if love.event then
+		love.event.pump()
+		for name, a,b,c,d,e,f in love.event.poll() do
+			if name == "keypressed" and a == "escape" and
+				(love.keyboard.isDown "lshift" or love.keyboard.isDown "rshift")
+			then
+				love.event.quit()
+			end
+			if name == "quit" then
+				if not quit or not quit() then
+					return
+				end
+			end
+		end
+	end
+
+	if love.timer then
+		love.timer.step()
+		dt = love.timer.getDelta()
+		if love.keyboard.isDown "tab" then
+			dt = dt * 4
+		end
+	end
+
+	if update then update(dt) end
+	if draw then draw() end
+
+	collectgarbage("step")
+
+	if love.timer then love.timer.sleep(0.001) end
+end
