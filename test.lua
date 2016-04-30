4c3c5cd98d519c39fa9474982e909ec19273763f
Colby Klein <shakesoda@gmail.com>
2016-04-30 13:15:06 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,27 +5,46 @@ local bgfx = require "bgfx"
 
 require "love"
 require "love.filesystem"
-love.filesystem.init(arg[0])
+
+local function get_low(a)
+	local m = math.huge
+	for k,v in pairs(a) do
+		if k < m then
+			m = k
+		end
+	end
+	return a[m]
+end
+love.filesystem.init(get_low(arg))
+love.filesystem.setSource(love.filesystem.getWorkingDirectory())
 
 bgfx.init(true)
 bgfx.reset(1280, 720, { "vsync" })
 bgfx.set_debug { "text" }
 
--- broken
--- local hmd = bgfx.get_hmd()
--- for k, v in pairs(hmd) do
--- 	print(k, v)
--- end
+local info = bgfx.get_renderer_info()
+print(info.name, info.type)
+
+local hmd = bgfx.get_hmd() or {}
+for k, v in pairs(hmd) do
+	print(k, v)
+end
 
 local fmt = bgfx.new_vertex_format {
 	{ attrib = "position", type = "float", num = 3 }
 }
+print(fmt)
+
+local ib = bgfx.new_index_buffer({1, 2, 3, 4, 5, 6})
+print(ib)
+
+local data, err = love.filesystem.newFileData("bin/bgfx.so")
+local vb = bgfx.new_vertex_buffer(data:getPointer(), data:getSize(), fmt)
+print(vb)
 
 bgfx.set_view_clear(0, { "color", "depth" }, 0x303030ff, 1.0, 0)
 bgfx.set_view_name(0, "igor");
 
--- local vb = bgfx.new_vertex_buffer()
--- local ib = bgfx.new_index_buffer()
 -- local vsh = bgfx.new_shader()
 -- local fsh = bgfx.new_shader()
 -- local csh = bgfx.new_shader()
@@ -44,5 +63,6 @@ for i=1,120 do
 
 	bgfx.frame()
 end
+
 
 bgfx.shutdown()
